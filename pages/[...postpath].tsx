281ffa5a80248b30ffbd1b14127a13907dbcfeaa
scarelettjohansson <146661662+scarelettjohansson@users.noreply.github.com>
2023-10-06 05:37:30 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					 'https://apkpure9.com/graphql.') + encodeURI(path as string)
+					'https://apkpure9.com/' + encodeURI(path as string)
 				}`,
 			},
 		};
